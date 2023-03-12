2da3e97c1b04177811f7890682ed874be58d6062
Palaniappan <ppalaniappan@prokarma.com>
2023-03-12 12:01:27 -0400

    Checking the gradle Build to see whether it s working for PollSCM and webhook"

---
+++ src/main/java/ThirdProjForGradle/ThirdProjecMain.java
@@ -0,0 +1,10 @@
+package ThirdProjForGradle;
+
+public class ThirdProjecMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Checking the gradle Build to see whether it s working for PollSCM and webhook");
+	}
+
+}
