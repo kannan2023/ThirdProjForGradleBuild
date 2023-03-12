72274ba05b7f89d75eb800f341b6315ed735921b
Palaniappan <ppalaniappan@prokarma.com>
2023-03-12 12:22:43 -0400

    Completed gradle build for Web Hook

--- src/main/java/ThirdProjForGradle/ThirdProjecMain.java
+++ src/main/java/ThirdProjForGradle/ThirdProjecMain.java
@@ -5,6 +5,8 @@ public class ThirdProjecMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Checking the gradle Build to see whether it s working for PollSCM and webhook");
+		System.out.println("Completed gradle build for Poll SCM");
+		System.out.println("Completed gradle build for Web Hook");
 	}
 
 }
